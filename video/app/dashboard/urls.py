a877c5e4b82344b6de547eefd32ed754029370b0
15637454123 <307798843@qq.com>
2020-02-26 21:17:28 +0800

    版本1.3（搭建dashboard后台，实现外联视频的信息添加及其播放地址）

--- video/app/dashboard/urls.py
+++ video/app/dashboard/urls.py
@@ -2,12 +2,17 @@
 
 from django.urls import path
 from .views.base import Index
-from .views.auth import Login, AdminManger,Logout
+from .views.auth import Login, AdminManger, Logout, UpdateAdminStatus
+from .views.video import ExternaVideo, VideoSubT
 
 
 urlpatterns = [
     path('', Index.as_view(), name='dashboard_index'),
     path('login', Login.as_view(), name='login'),
     path('logout', Logout.as_view(), name='logout'),
-    path('admin/manager', AdminManger.as_view(), name='admin_manager')
+    path('admin/manager', AdminManger.as_view(), name='admin_manager'),
+    path('admin/manager/update/status', UpdateAdminStatus.as_view(), name='update_admin_status'),
+    path('video/externa', ExternaVideo.as_view(), name='externa_video'),
+    path('video/videosub/<int:video_id>', VideoSubT.as_view(), name='video_sub')
+
 ]
--- video/app/dashboard/views/auth.py
+++ video/app/dashboard/views/auth.py
@@ -5,6 +5,8 @@ from django.shortcuts import redirect, reverse
 from app.libs.base_render import render_to_response
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator
+from app.utils.permission import dashboard_auth
 
 
 class Login(View):
@@ -15,7 +17,9 @@ class Login(View):
         if request.user.is_authenticated:
             return redirect(reverse('dashboard_index'))
 
-        data = {'error': ''}
+        to = request.GET.get('to', '')
+
+        data = {'error': '', 'to': to}
 
         return render_to_response(request, self.TEMPLATE, data=data)
 
@@ -23,6 +27,8 @@ class Login(View):
 
         username = request.POST.get('username')
         password = request.POST.get('password')
+
+        to = request.GET.get('to', '')
 
         data = {}
         exists = User.objects.filter(username=username).exists()
@@ -42,6 +48,9 @@ class Login(View):
             return render_to_response(request, self.TEMPLATE, data=data)
 
         login(request, user)
+        if to:
+            return redirect(to)
+
         return redirect(reverse('dashboard_index'))
 
 
@@ -55,10 +64,40 @@ class Logout(View):
 class AdminManger(View):
     TEMPLATE = 'dashboard/auth/admin.html'
 
+    @dashboard_auth
     def get(self, request):
 
-        users = User.objects.filter(is_superuser=True)
-        data = {'users': users}
+        users = User.objects.all()
+
+        # 分页
+        page = request.GET.get('page', 1)
+        p = Paginator(users, 2)
+        total_page = p.num_pages  # 取得分页数量
+
+        # 防止反序
+        if int(page) <= 1:
+            page = 1
+
+        current_page = p.get_page(int(page)).object_list  # 把user对象，放到list列表中
+
+        data = {'users': current_page, 'total': total_page, 'page_num': int(page)}
 
         return render_to_response(request, self.TEMPLATE, data=data)
+
+
+class UpdateAdminStatus(View):
+
+    def get(self, request):
+
+        status = request.GET.get('status', 'on')
+        print(status)
+        _status = True if status == 'on' else False
+        print(_status)
+        request.user.is_superuser = _status
+        request.user.save()
+
+        return redirect(reverse('admin_manager'))
+
+
+
 
